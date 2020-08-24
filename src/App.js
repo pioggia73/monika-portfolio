9af9d786c1bbeb55123498bc99f9707c94a95ccf
Monika Wasilewska <monikawasilewska13@wp.pl>
2020-08-24 18:47:29 +0200

    adding lazy

--- src/App.js
+++ src/App.js
@@ -1,35 +1,36 @@
-import React, { Component } from "react";
+import React, { lazy, Component, Suspense } from "react";
 import {Route, Switch} from 'react-router-dom';
 
-import Home from "./pages/Home";
 import Default from "./pages/Default";
 import Navbar from "../src/components/Navbar";
 import Sidebar from "../src/components/Sidebar";
-import CoursesPage from "../src/pages/CoursesPage";
-import ContactPage from "../src/pages/ContactPage";
-import PortfolioPage from "../src/pages/PortfolioPage"
-
 import GlobalStyles from "./components/globals/GlobalStyles";
+
+const Home = lazy(() => import('./pages/Home'));
+const CoursesPage = lazy(() => import('../src/pages/CoursesPage'));
+const ContactPage = lazy(() => import('../src/pages/ContactPage'));
+const PortfolioPage = lazy(() => import('../src/pages/PortfolioPage'));
+
 
 class App extends Component {
   render() {
     return (
       <React.Fragment>
-        
         <GlobalStyles />
         <Navbar />
         <Sidebar />
         <Switch>
-            <Route exact path= "/" component={Home} />
-            <Route exact path= "/courses" component={CoursesPage} />
-            <Route exact path= "/contact" component={ContactPage} />
-            <Route exact path= "/portfolio" component={PortfolioPage} />
-            <Route component = {Default} />
+            <Suspense fallback={<div>...Loading </div>}>
+              <Route exact path= "/" component={Home} />
+              <Route exact path= "/courses" component={CoursesPage} />
+              <Route exact path= "/contact" component={ContactPage} />
+              <Route exact path= "/portfolio" component={PortfolioPage} />
+            </Suspense>
+              <Route component = {Default} />
         </Switch>
-        
-      </React.Fragment>
+      </React.Fragment>  
     );
   }
-}
+};
 
 export default App;
